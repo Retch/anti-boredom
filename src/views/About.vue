8cc92510ce8536accf9a19c34cf742766d36e05d
Retch <retch100@gmail.com>
2021-03-16 17:42:52 +0100

    Styled About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="about" style="margin: 1rem">
+  <div class="about" style="margin: 1rem; max-width: 500px">
     <p>
-      If you are bored and have no idea what to do,
-      just press the button. Magically,
-      an activity will be suggested to you and the chart will show how many people should participate in it,
-      how expensive the activity could be and how accessibile it is.
+      If you are bored and have no idea what to do, just press the button. Magically, an
+      activity will be suggested to you and the chart will show how many people should
+      participate in it, how expensive the activity could be and how accessibile it is.
       Just a little App which consumes the API from
       <a href="https://www.boredapi.com">www.boredapi.com</a> and shows the data in a
       fancy way.
