587cf15ad4aca14a5e0f945c11cbb55aed1e17cb
Retch <retch100@gmail.com>
2021-03-16 12:36:06 +0100

    Fixed spelling

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <p>
       Just a little App which consumes the API from
       <a href="https://www.boredapi.com">www.boredapi.com</a> and shows the data in a
-      fance way.
+      fancy way.
     </p>
   </div>
 </template>
